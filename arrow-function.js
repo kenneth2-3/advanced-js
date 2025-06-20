6c324915bf1472dd1b77b7455b177a7ff1a03fcb
kenneth2-3 <125259470+kenneth2-3@users.noreply.github.com>
2025-06-20 13:15:04 +0000

    Add other functions explaining the arrow function method

--- arrow-function.js
+++ arrow-function.js
@@ -7,10 +7,51 @@
 // let sum = addTwoNumbers(3, 5);
 // console.log(sum);
 
+function addTwoLetters(c, d) {
+    return c + d;
+}
+let add = addTwoLetters(9, 10);
+console.log(add);
+
 // Arrow Function With Parameters (ES6)
+
 const addTwoNumbers = (a, b) => {
     // code block
     return a + b;
 }
 let sum = addTwoNumbers(3, 5);
 console.log(sum);
+
+const addTwoFigures = (e, f) => {
+    return e + f;
+}
+let plus = addTwoFigures(2, 4);
+console.log(plus);
+
+// Single Line Arrow Function With Parameters
+
+// const addTwoNumbers2 = (a, b) => (a + b); also a valid way to write it
+const addTwoNumbers2 = (a, b) => a + b;
+let sum2 = addTwoNumbers2(6,4);
+console.log(sum2);
+
+const addNum = (g, h) => g + h;
+let num = addNum(1, 2);
+console.log(num);
+
+// Implicit Returns
+
+const saySomething = message => console.log(message);
+saySomething('Hello there!!'); 
+
+const sayHello = () => console.log('hello');  // for when you have more than one message to pass
+sayHello();
+
+// Returning Multiple Lines
+
+const returnMultipleLines = () => (
+    `<p>
+      This is a multiline string!
+    </p>`
+)
+console.log(returnMultipleLines())
