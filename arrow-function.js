be4832e1f22971b27d10e15140f7a08299fa2347
kenneth2-3 <125259470+kenneth2-3@users.noreply.github.com>
2025-06-20 12:44:51 +0000

    Create a function using the arrow function method

--- arrow-function.js
+++ arrow-function.js
@@ -1,5 +1,14 @@
 // Vanilla Javascript Function
-function addTwoNumbers(a, b) {
+
+// function addTwoNumbers(a, b) {
+//     // code block
+//     return a + b;
+// }
+// let sum = addTwoNumbers(3, 5);
+// console.log(sum);
+
+// Arrow Function With Parameters (ES6)
+const addTwoNumbers = (a, b) => {
     // code block
     return a + b;
 }
