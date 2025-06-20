2088f39c573fc0a5e865265378af94f8698ccc06
kenneth2-3 <125259470+kenneth2-3@users.noreply.github.com>
2025-06-20 16:22:40 +0000

    Add functions explaining the destructuring method

--- destructuring.js
+++ destructuring.js
@@ -1,10 +1,55 @@
 // Destructuring arrays
-
+let ages = [30, 26, 27];
+// let john = ages[0];
+// let mary = ages[1];
+// let joe = ages[2];
+let [john, mary, joe] = ages;
+console.log(john, mary, joe);
 
 // Destructuring objects
-
+let jobs = {
+    mike: "designer",
+    jill: "developer",
+    alicia: "accountant",
+};
+let { mike, jill, alicia } = jobs;
+console.log(mike, jill, alicia);
 
 // Destructuring subsets
+let languages = ["english", "french", "spanish", "german", "japanese"];
+let [johnNative, JohnSecondary] = languages;
+console.log(johnNative, JohnSecondary);
 
+let [, , maryNative, marySecondary] = languages;
+console.log(maryNative, marySecondary);
+
+let [, , , , joeNative] = languages;
+console.log(joeNative);
+
+let languages2 = {
+    firstLanguage: "english",
+    secondLanguage: "french",
+    thirdLanguage: "german",
+    fourthLanguage: "japanese",
+};
+let { firstLanguage, thirdLanguage } = languages2;
+console.log(firstLanguage, thirdLanguage);
 
 // Using rest parameter syntax
+
+let fruits = ["apple", "orange", "banana", "peach", "cherry"];
+let [favorite, secondFavorite, ...others] = fruits;
+console.log(favorite);
+console.log(secondFavorite);
+console.log(others);
+
+let favoritefoods = {
+    brian: "pizza",
+    anna: "pasta",
+    sarah: "vegetarian",
+    andrea: "steak"
+};
+let { brian, anna, ...rest } = favoritefoods;
+console.log(brian);
+console.log(anna);
+console.log(rest);
