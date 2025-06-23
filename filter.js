96f4f0cbb3bd5a0f7f22c1a660082aab4d5d858d
kenneth2-3 <125259470+kenneth2-3@users.noreply.github.com>
2025-06-23 14:50:38 +0000

    Create a new file called reduce.js

--- filter.js
+++ filter.js
@@ -14,3 +14,56 @@ const people = [
         age: 18,
     },
 ];
+
+const oldEnough = people.filter(person => person.age >= 21);
+console.log(oldEnough);
+
+const paul = people.filter(p => p.name === "Paul");
+console.log(paul);
+
+// Complex filtering
+
+const students = [
+    {
+        id: 1,
+        name: 'Mark',
+        profession: 'Developer',
+        skills: [
+            { name: 'javascript', yrsExperience: 1 },
+            { name: 'html', yrsExperience: 5 },
+            { name: 'css', yrsExperience: 3 },
+        ]
+    },
+    {
+        id: 2,
+        name: 'Ariel',
+        profession: 'Developer',
+        skills: [
+            { name: 'javascript', yrsExperience: 0 },
+            { name: 'html', yrsExperience: 4 },
+            { name: 'css', yrsExperience: 2 },
+        ]
+    },
+    {
+        id: 3,
+        name: 'Jason',
+        profession: 'Designer',
+        skills: [
+            { name: 'javscript', yrsExperience: 1 },
+            { name: 'html', yrsExperience: 1 },
+            { name: 'css', yrsExperience: 5 },
+        ]
+    },
+];
+
+const candidates = students.filter(student => {
+    let strongSkills = student.skills.filter(skill => skill.yrsExperience >= 5);
+    return strongSkills.length > 0;
+});
+console.log(candidates);
+
+// for a cleaner way
+const has5yearsExp = skill => skill.yrsExperience >= 5;
+const hasStrongSkills = student => student.skills.filter(has5yearsExp).length > 0;
+const candidates1 = students.filter(hasStrongSkills);
+console.log(candidates1);
