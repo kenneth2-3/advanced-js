0770475d2a6c0096bb399eb43d47077e6d21acca
kenneth2-3 <125259470+kenneth2-3@users.noreply.github.com>
2025-06-24 11:44:13 +0000

    Create a file for explaining apis

---
+++ api.js
@@ -0,0 +1,9 @@
+let bookCharacter = {
+    name: "Reginald Jeeves",
+    employer: "Bertram Wilberforce Wooster",
+    novels: 11,
+    shortStories: 35
+}
+
+console.log(bookCharacter.employer);
+console.log(bookCharacter["employer"]);
